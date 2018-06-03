c922f78c8c226ee5aaee7edace32059f28ff0f7d
Levi Durfee <levi.durfee@gmail.com>
2018-06-02 21:15:39 -0400

    get it working properly

--- handlers.go
+++ handlers.go
@@ -7,6 +7,8 @@ import (
 )
 
 func ipHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s", r.Header["x-forwarded-for"])
-	log.Println(r.Header["x-forwarded-for"])
+	ip := r.Header.Get("x-forwarded-for")
+	fmt.Fprintf(w, "%s", ip)
+	log.Println(ip)
 }
+
