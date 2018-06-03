ee9850ce683d016baaee9ff58e6c78fdb0d25e17
Levi Durfee <levi.durfee@gmail.com>
2018-06-02 21:04:07 -0400

    return xfwdfor

--- handlers.go
+++ handlers.go
@@ -7,6 +7,6 @@ import (
 )
 
 func ipHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s", r.RemoteAddr)
+	fmt.Fprintf(w, "%s", r.Header["x-forwarded-for"])
 	log.Println(r.Header["x-forwarded-for"])
 }
